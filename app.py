7db2f48ee4939873cb56ea5091e5c588ca299512
dennismuturia <dmuturia2922@gmail.com>
2018-04-24 14:36:01 +0300

    This is the basic structure on the Flask framework. I have defined the root 'home page' by using the foward slash and returned a string .

--- app.py
+++ app.py
@@ -3,3 +3,14 @@
 * This is just a basic bot for fun and to play with
 * Enjoy 
 '''
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+
+def receive_Message():
+    return("Hello world")
+
+if __name__ == '__main__':
+    app.run()
