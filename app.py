d357510d1de5856181a8d6f7672cf1497bb82706
dennismuturia <dmuturia2922@gmail.com>
2018-04-24 15:05:02 +0300

    If the request was not get, it must be POST and we can just proceed with sending a message # back to user

--- app.py
+++ app.py
@@ -18,5 +18,24 @@ def receive_Message():
         '''
         token_sent = requests.args.get('hub.verify_token')
         return verify_fb_token
+    
+    #If the request was a post request then procedd with sending a message back to the user
+    else:
+        #Getting the message the user sent the bot
+        output = request.get_json()
+        for event in output:
+            messaging = event['messaging']
+            for message in messaging:
+                if message.get('message'):
+                    #Facebook Messenger ID for user so we know where to send response back to
+                    recepient_id = message['sender']['id']
+                    if message['message'].get('text'):
+                        response_sent_text = get_message():
+                        send_message(recepient_id, response_sent_text)
+                    #If a user send a photo, Video of a media file
+                    if message['message'].get('attachments'):
+                        response_sent_nontext = get_message()
+                        send_message(recipient_id, response_sent_nontext)
+    return "Message processed"
 if __name__ == '__main__':
     app.run()
